14a373ea4914f6a6694cb24836a6e14eece4544b
adamchahoud <adamchahoud0@gmail.com>
2026-01-01 04:52:37 +0100

    Initial project skeleton (Spring Boot, Docker, Flyway, CI)

---
+++ src/test/java/com/adam/opsflow/OpsflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adam.opsflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpsflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
